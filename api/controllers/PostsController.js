473c97852577d3740b7576c6feb95f072db1b132
Anthony Valle <valleganthony@gmail.com>
2019-08-09 19:24:36 -0400

    Minor changes

--- api/controllers/PostsController.js
+++ api/controllers/PostsController.js
@@ -1,18 +1,44 @@
+ //dummy DB
+const post1= {id:1, 
+ 			title:'Post title', 
+ 			body:'Here is my post text.This is cool!'}
+const post2= {id:2, 
+			title:'Post title Two', 
+ 			body:'some body text'}
+const post3= {id:3, 
+			title:'Post title Two', 
+ 			body:'some body text'}	 				
+
+const allPost = [post1,post2,post3]
+
  module.exports = {
  	
  	posts: function(req, res){
- 		const post1= {id:1, 
- 			title:'Post title', 
- 			body:'Here is my post text.This is cool!'}
- 		const post2= {id:2, 
- 			title:'Post title Two', 
- 			body:'some body text'}	
+ 		
+ 		res.send([allPost])
+ 	},
 
- 		res.send([post1,post2])
+ 	create: function(req, res){
+ 		 const title = req.param('title')
+ 		 const body = req.param('body')
+
+ 		const newPosts = {id:4, title: title, body:body}
+ 		console.log(title +' '+ body )
+ 		allPost.push(newPosts)
+
+ 		res.end()
  	},
 
  	findById: function(req, res){
  		const postID = req.param('postId') 
- 		res.send(postID)
- 	}
+ 		
+ 		const filteredPosts = allPost.filter(p =>{return p.id == postID})
+ 		if (filteredPosts.length > 0) {
+ 			res.send(filteredPosts[0])
+ 		}else{
+ 			res.send('Failed to find post by id: ' + postID)
+ 		}
+ 	}   
+
+
  }
--- config/routes.js
+++ config/routes.js
@@ -22,7 +22,8 @@ module.exports.routes = {
   '/': { view: 'pages/homepage' },
 
   '/posts': 'PostsController.posts',
-  '/findById/:postId': 'PostsController.findById'
+  '/findById/:postId': 'PostsController.findById', 
+  '/create': 'PostsController.create'
 
 
   /***************************************************************************
